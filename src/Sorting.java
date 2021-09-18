3175839d5a686c38fb9637790529bde9e652fd8e
Tanner Wills <tannerwills1@gmail.com>
2021-09-18 16:58:17 -0400

    dd

---
+++ src/Sorting.java
@@ -0,0 +1,104 @@
+import java.util.Comparator;
+
+/**
+ * Your implementation of various iterative sorting algorithms.
+ */
+public class Sorting {
+
+    /**
+     * Implement bubble sort.
+     *
+     * It should be:
+     * in-place
+     * stable
+     * adaptive
+     *
+     * Have a worst case running time of: O(n^2)
+     * And a best case running time of: O(n)
+     *
+     * NOTE: You should implement bubble sort with the last swap optimization.
+     *
+     * You may assume that the passed in array and comparator
+     * are both valid and will never be null.
+     *
+     * @param <T>        Data type to sort.
+     * @param arr        The array that must be sorted after the method runs.
+     * @param comparator The Comparator used to compare the data in arr.
+     */
+    public static <T> void bubbleSort(T[] arr, Comparator<T> comparator) {
+        // WRITE YOUR CODE HERE (DO NOT MODIFY METHOD HEADER)!
+        /**
+         * stopIndex becomes array.length -1
+         * while( stopIndex > 0){
+         *     initialize inner loop index ,i
+         *     while( i < stopIndex){
+         *         if( array[i] > array[i+1])
+         *             swap values at i and i+1
+         *         increment i
+         *     decrement stopIndex
+         */
+
+        /** Optimization 1 & 2 (if no swaps are needed for an entire iteration, early termination)
+         * (Add swapsMade boolean variable)
+         * (track index of when last swap occurred)
+         * stopIndex becomes array.length -1
+         * while( stopIndex > 0){
+         *     initialize lastSwapped to mark swap index
+         *     initialize inner loop index ,i
+         *     while( i < stopIndex){
+         *         if( array[i] > array[i+1])
+         *             swap values at i and i+1
+         *             lastSwapped = i
+         *         increment i
+         *     stopIndex = lastSwapped
+         */
+
+
+
+
+    }
+
+    /**
+     * Implement selection sort.
+     *
+     * It should be:
+     * in-place
+     * unstable
+     * not adaptive
+     *
+     * Have a worst case running time of: O(n^2)
+     * And a best case running time of: O(n^2)
+     *
+     * You may assume that the passed in array and comparator
+     * are both valid and will never be null.
+     *
+     * @param <T>        Data type to sort.
+     * @param arr        The array that must be sorted after the method runs.
+     * @param comparator The Comparator used to compare the data in arr.
+     */
+    public static <T> void selectionSort(T[] arr, Comparator<T> comparator) {
+        // WRITE YOUR CODE HERE (DO NOT MODIFY METHOD HEADER)!
+    }
+
+    /**
+     * Implement insertion sort.
+     *
+     * It should be:
+     * in-place
+     * stable
+     * adaptive
+     *
+     * Have a worst case running time of: O(n^2)
+     * And a best case running time of: O(n)
+     *
+     * You may assume that the passed in array and comparator
+     * are both valid and will never be null.
+     *
+     * @param <T>        Data type to sort.
+     * @param arr        The array that must be sorted after the method runs.
+     * @param comparator The Comparator used to compare the data in arr.
+     */
+    public static <T> void insertionSort(T[] arr, Comparator<T> comparator) {
+        // WRITE YOUR CODE HERE (DO NOT MODIFY METHOD HEADER)!
+    }
+}
